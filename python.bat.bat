24e07b93cb82727e0ab29c05a95728bd5a960c47
Adoorsatish <91518810+Adoorsatish@users.noreply.github.com>
2021-10-01 07:43:32 +0530

    Add files via upload
    Python bat files

---
+++ python.bat.bat
@@ -0,0 +1,13 @@
+rem change drive ::
+cd /D D:
+
+rem change working directory to your intended folder
+rwm ===============================================
+rem below path should be changed before running the script.
+rem file must be saved as .bat file eg := "mybat.bat" in the file name section with double quotes with file type choosen as All files.
+rem Once done save and copy it to desktop and double click , code will be executed and directed to required path as shown below
+rem cd work\python\mypython
+ cd work\python\IPBA
+
+rem lauch jupiter notebook
+jupyter notebook
